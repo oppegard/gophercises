6d576c5cb2ee10c77bad60c4d0c2ae8a56e06c05
Glenn Oppegard <oppegard@gmail.com>
2020-01-11 13:42:32 -0700

    quiz: part 1 partial solution
    It does not yet take a CLI option to specify a CSV file.

---
+++ quiz/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	correct := 0
+	for i, record := range records {
+		question, answer := record[0], record[1]
+		fmt.Printf("Problem #%d: %s = ", i+1, question)
+
+		var input string
+		n, err := fmt.Scanln(&input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n != 1 {
+			log.Fatal("Only single-word answer are allowed.")
+		}
+
+		if input == answer {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(records))
+}
